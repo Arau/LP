462a560489d954c5a5769dd86bbf2169f57cfe53
Ferran Arau <ferran.arau.castell@gmail.com>
2013-10-02 10:42:16 +0200

    First implementation of AVG

--- llistes.hs
+++ llistes.hs
@@ -6,3 +6,12 @@ my_max (x:xs)
     | otherwise = y
     where y = my_max(xs)
 
+my_avg []   = 0
+my_avg [x]  = x
+my_avg  l   = my_sum l `div` my_count l
+
+my_sum [x]      = x
+my_sum (x:xs)   = x + my_sum xs
+
+my_count []     = 0
+my_count (x:xs) = 1 + my_count(xs)
