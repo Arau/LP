a610e51eef24ed95da8ff08fb7c6f4869668c33a
Ferran Arau <ferran.arau.castell@gmail.com>
2013-11-05 11:40:04 +0100

    Compare 2 trees

--- Haskell/dataTypes/opsArbre.hs
+++ Haskell/dataTypes/opsArbre.hs
@@ -16,8 +16,15 @@ mostrarArbre (Node x a1 a2) m = x:(mostrarArbre a1 m)++(mostrarArbre a2 m)
 
 prova  = (mostrarArbre (fst (obtenirArbre [3,2,0,0,4,5,0,0,0] 0)) 0)
 
+
 myTree = fst (obtenirArbre [3,2,0,0,4,5,0,0,0] 0)
 
 highTree :: Arbre a -> Int
 highTree Abuit = 0
 highTree tree  = 1 + max (highTree $ fe tree) (highTree $ fd tree)
+
+cmpTree :: Eq a => Arbre a -> Arbre a -> Bool
+cmpTree Abuit Abuit = True
+cmpTree _     Abuit = False
+cmpTree Abuit _     = False
+cmpTree (Node x cl1 cr1) (Node y cl2 cr2) = x==y && (cmpTree cl1 cl2) && (cmpTree cr1 cr2)
