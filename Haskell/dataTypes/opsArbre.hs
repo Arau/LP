8c426b8d6126b2c115fa9356864f17bc8216150d
Ferran Arau <ferran.arau.castell@gmail.com>
2013-11-09 08:31:28 +0100

    Tree operations. Preord, inord and postord.

--- Haskell/dataTypes/opsArbre.hs
+++ Haskell/dataTypes/opsArbre.hs
@@ -14,10 +14,9 @@ obtenirArbre (x:xs) m = if (x==m) then (abuit,xs)
 mostrarArbre Abuit m = [m]
 mostrarArbre (Node x a1 a2) m = x:(mostrarArbre a1 m)++(mostrarArbre a2 m)
 
-prova  = (mostrarArbre (fst (obtenirArbre [3,2,0,0,4,5,0,0,0] 0)) 0)
+prova  = (mostrarArbre (fst (obtenirArbre [3,2,5,0,4,0,0,8,1,6,0,0,7,0,0,2,0,0,10,1,0,0,5,0,6,1,2,0,0,5,0,0,3,0,9,10,0,0,0] 0)) 0)
 
-
-myTree = fst (obtenirArbre [3,2,0,0,4,5,0,0,0] 0)
+myTree = fst (obtenirArbre [3,2,5,0,4,0,0,8,1,6,0,0,7,0,0,2,0,0,10,1,0,0,5,0,6,1,2,0,0,5,0,0,3,0,9,10,0,0,0] 0)
 
 highTree :: Arbre a -> Int
 highTree Abuit = 0
@@ -28,3 +27,15 @@ cmpTree Abuit Abuit = True
 cmpTree _     Abuit = False
 cmpTree Abuit _     = False
 cmpTree (Node x cl1 cr1) (Node y cl2 cr2) = x==y && (cmpTree cl1 cl2) && (cmpTree cr1 cr2)
+
+
+-- go over through the tree
+
+preord Abuit    = []
+preord (Node x chl chr)  = [x] ++ (preord chl) ++ (preord chr)
+
+inord Abuit     = []
+inord (Node x chl chr)   = (inord chl) ++ [x] ++ (inord chr)
+
+postord Abuit   = []
+postord (Node x chl chr) = (postord chl) ++ (postord chr) ++ [x]
