ae8626299ba1ff17d83441235a0db21c6b09b891
Ferran Arau <ferran.arau.castell@gmail.com>
2013-11-10 12:46:22 +0100

    Implementing instance of Show because printing Tree

--- Haskell/dataTypes/Arbre.hs
+++ Haskell/dataTypes/Arbre.hs
@@ -3,7 +3,7 @@ module Arbre where
 {-- Arbres binaris amb arbres buits --}
 data Arbre a = Node a (Arbre a) (Arbre a)
              | Abuit
- deriving (Eq,Show)
+ deriving (Eq)
 
 abuit = Abuit
 
@@ -17,3 +17,19 @@ fd (Node _ _ a2) = a2
 esbuit Abuit = True
 esbuit (Node _ _ _) = False
 
+-- Show
+
+instance Show a => Show (Arbre a) where
+    show tree = sTree tree 0
+
+sTree :: (Show a, Eq n, Num n) => Arbre a -> n -> String
+sTree  Abuit level = (tab level) ++ "_"
+sTree (Node x chl chr) l = (tab l) ++ (show  x)
+                        ++ "\n" ++ (tab l) ++ (sTree chl i)
+                        ++ "\n" ++ (tab l) ++ (sTree chr i)
+                        where i = sum [l,1]
+
+tab :: (Eq a, Num a) => a -> [Char]
+tab 0 = ""
+tab n = "  " ++ (tab y)
+    where y = subtract 1 n
